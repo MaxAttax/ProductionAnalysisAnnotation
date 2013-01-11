4edf398cbfe4001fc28c99bd7f03c0661a91486b
MaxAttax <maxattaxhoffmann@gmail.com>
2013-01-11 17:48:03 +0100

    first commit

---
+++ src/org/maxhoffmann/dev/ProductionAnalysisAnnotation/RunAnalysis.java
@@ -0,0 +1,9 @@
+package org.maxhoffmann.dev.ProductionAnalysisAnnotation;
+
+public class RunAnalysis {
+
+	public static void main(String[] args) {
+		System.out.println("Let's start the Analysis!");
+	}
+	
+}
